ffbf45ca6934930edf56466f5c63cb848e457231
edu.f <edu.f@com>
2022-04-27 10:40:57 +0200

    clase 27/04 - commit Edu

--- src/test/java/ObtenerPokemonTest.java
+++ src/test/java/ObtenerPokemonTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ObtenerPokemonTest {
@@ -14,26 +15,40 @@ public class ObtenerPokemonTest {
     @Test
     public void probarPokemon3Nombre(){
         // TODO Comprueba que al preguntar por Pokémon con id 3 obtienes a venasaur.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getName(),"venusaur");
     }
 
     @Test
     public void probarPokemon3Id(){
-        // TODO Comprueba que al preguntar por Pokémon con venasaur obtienes el id 2.
+        // TODO Comprueba que al preguntar por Pokémon con venusaur obtienes el id 3.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getName(),"venusaur");
     }
 
     @Test
     public void probarPokemon3Peso(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes el peso de 1000.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getWeight(),"1000");
     }
 
     @Test
     public void probarPokemon3Altura(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes la altura de 20.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get("20");
+        Assertions.assertEquals(pokemon3.getId(),3);
     }
 
     @Test
     public void probarPokemon4a6(){
         // TODO Comprueba que los Pokémon (id = 4, id = 5, id = 6) obtienes a charmander, charmeleon y charizard.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(4);
+        Assertions.assertEquals(pokemon3.getName(),"charmander");
+        Pokemon pokemon4 = ObtenerPokemonRequest.Companion.get(5);
+        Assertions.assertEquals(pokemon4.getName(),"charmeleon");
+        Pokemon pokemon5 = ObtenerPokemonRequest.Companion.get(6);
+        Assertions.assertEquals(pokemon5.getName(),"charizard");
     }
 
     @Test
