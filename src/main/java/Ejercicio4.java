a3d39190afb3b3dc9bba52f7533bb6c81a6a7670
edu.f <edu.f@com>
2022-05-10 10:19:19 +0200

    clase 04/05 -ej4 pokemonMasGordosQue

--- src/main/java/Ejercicio4.java
+++ src/main/java/Ejercicio4.java
@@ -1,31 +1,40 @@
 public class Ejercicio4 {
 
-        public Pokemon[] arrayPokemons = new Pokemon[0];
+    public Pokemon[] arrayPokemons = new Pokemon[0];
 
-        public void iniciarEjercicio4 () {
-            System.out.println("Empezando el ej4");
-            for (Pokemon pokemon : arrayPokemons) {
-                System.out.println(pokemon);
-            }
-
-            System.out.println("Resultado de pokemonMasGordoQue");
-            Pokemon[] result = pokemonMasGordosQue(225);
-            for (Pokemon pokemon : result) {
-                System.out.println(pokemon);
-            }
+    public void iniciarEjercicio4() {
+        System.out.println("Empezando el ej4");
+        for (Pokemon pokemon : arrayPokemons) {
+            System.out.println(pokemon);
         }
 
-        public Pokemon[] pokemonMasGordosQue (Integer peso){
-            Pokemon[] out = new Pokemon[arrayPokemons.length];
-            int i = 0;
-            for (Pokemon pokemon : arrayPokemons) {
-                if (pokemon.getWeight() > peso) {
-                    out[i] = pokemon;
-                    i++;
-                }
-
-            }
-            return out;
+        System.out.println("Resultado de pokemonMasGordoQue");
+        Pokemon[] result = pokemonMasGordosQue(225);
+        for (Pokemon pokemon : result) {
+            System.out.println(pokemon);
         }
-
     }
+
+
+    public Pokemon[] pokemonMasGordosQue(Integer peso) {
+        Pokemon[] out = new Pokemon[arrayPokemons.length];
+        int i = 0, j = 0;
+        for (Pokemon pokemon : arrayPokemons) {
+            if (pokemon.getWeight() > peso) {
+                out[i] = pokemon;
+                i++;
+            }
+        }
+
+        Pokemon[] outWithoutNull = new Pokemon[i];
+        i=0;
+        for (j=0;j<=outWithoutNull.length;j++) {
+            if(out[j]!=null) {
+                outWithoutNull[j] = out[j];
+            }
+        }
+        return outWithoutNull;
+    }
+
+}
+
